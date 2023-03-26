f1ccee6c15da52f45596894d298f6a83d9296c74
Murasakino <120464393+FireDragonGod@users.noreply.github.com>
2023-03-26 14:56:32 +0800

    Create a default 16x16 div in the square

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const helloWorld = 'Hello, World!';
+//
+//
+//
+const square = document.querySelector('#square');
+const gridSizeFromDom = document.querySelector('.gridSize');
+const grid = document.querySelector('.grid');
+const colorPicker = document.querySelector('#color-picker');
+const colorMode = document.querySelector('.color-mode');
+const clear = document.querySelector('.clear');
+const randomColor = document.querySelector('.random');
+
+const divCreation = function createLotsOfDivInSquareContainer() {
+  let grid = 16;
+  let i = 0;
+  for (; i < (grid * grid); i++) {
+    const divCreate = document.createElement('div');
+    square.style.cssText = `display: grid; grid-template-columns: repeat(${grid}, 1fr); grid-template-rows: repeat(${grid}, 1fr);`;
+    square.appendChild(divCreate);
+  }
+  gridSizeFromDom.textContent = `${grid} x ${grid}`;
+};
+
+divCreation();
